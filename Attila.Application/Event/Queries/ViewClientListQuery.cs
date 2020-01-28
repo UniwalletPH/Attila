3d24042f0108acb28974edf27a46223cf684a188
Anthony Jasper Dalit <dalitanthonyjasper@gmail.com>
2020-01-28 21:49:17 +0800

    client query

--- Attila.Application/Event/Queries/ViewClientListQuery.cs
+++ Attila.Application/Event/Queries/ViewClientListQuery.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Attila.Application.Interfaces;
+using Attila.Domain.Entities.Tables;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Attila.Application.Event.Queries
 {
-    public class ViewClientListQuery 
+    public class ViewClientListQuery : IRequest<List<EventClient>>
     {
+        public ViewClientListQuery()
+        {
+        
+        }
+
+        public class ViewClientListQueryHandler : IRequestHandler<ViewClientListQuery, List<EventClient>>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public ViewClientListQueryHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public Task<List<EventClient>> Handle(ViewClientListQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
     }
 }
