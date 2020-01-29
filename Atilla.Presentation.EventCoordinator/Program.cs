95ae5d02d81e757512a34f6703338b0cf8b2af7f
Roy Paolo Real <rpreal99@gmail.com>
2020-01-29 17:03:47 +0800

    Event Coordinator Console

---
+++ Atilla.Presentation.EventCoordinator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atilla.Presentation.EventCoordinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
