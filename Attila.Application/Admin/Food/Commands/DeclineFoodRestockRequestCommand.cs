45beb921a5b59782be974f55acfac670040cb44a
Adrian Surla <adrian.surla@uniwallet.ph>
2020-01-29 14:57:47 +0800

    restructured application folders

---
+++ Attila.Application/Admin/Food/Commands/DeclineFoodRestockRequestCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Application.Admin.Food.Commands
+{
+    class DeclineFoodRestockRequestCommand
+    {
+    }
+}
