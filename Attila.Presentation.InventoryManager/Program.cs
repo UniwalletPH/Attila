c7bc039873de0b4434cab4414ee31ee81d630914
Adrian Surla <adrian.surla@uniwallet.ph>
2020-01-29 16:08:55 +0800

    Presentation - Inventory Manager

---
+++ Attila.Presentation.InventoryManager/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Presentation.InventoryManager
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
