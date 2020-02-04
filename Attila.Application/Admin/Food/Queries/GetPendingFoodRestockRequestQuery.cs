2ed5700bd64a227bc983bd9b1cc35d86300af042
Anthony Jasper Dalit <dalitanthonyjasper@gmail.com>
2020-02-04 17:01:06 +0800

    sa

--- Attila.Application/Admin/Food/Queries/GetPendingFoodRestockRequestQuery.cs
+++ Attila.Application/Admin/Food/Queries/GetPendingFoodRestockRequestQuery.cs
@@ -3,6 +3,7 @@ using Attila.Domain.Entities;
 using Attila.Domain.Enums;
 using MediatR;
 using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -22,7 +23,8 @@ namespace Attila.Application.Admin.Food.Queries
 
             public async  Task<List<FoodRestockRequest>> Handle(GetPendingFoodRestockRequestQuery request, CancellationToken cancellationToken)
             {
-                var _pendingFoodRestock = dbContext.FoodRestockRequests                 
+                var _pendingFoodRestock = dbContext.FoodRestockRequests
+                    .Include(a => a.FoodDetails)
                     .Where(a => a.Status == Status.Pending);
 
                 return _pendingFoodRestock.ToList();
