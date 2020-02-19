b90724387e12c0c93e5b7bdb8503e84d2224b8bf
Anthony Jasper Dalit <dalitanthonyjasper@gmail.com>
2020-02-19 16:52:44 +0800

    Additional

--- Attila.UI/Controllers/AdminController.cs
+++ Attila.UI/Controllers/AdminController.cs
@@ -1,14 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Attila.Application.Admin.Event.Queries;
+using Attila.UI.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Attila.UI.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IMediator mediator;
+
+        public AdminController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Event()
+        {
+            var _pendingEvents = await mediator.Send(new GetAllPendingEventsQuery { });
+            var _incomingEvents = await mediator.Send(new GetAllIncomingEventsQuery { });
+            var _pastEvents = await mediator.Send(new GetAllPastEventsQuery { });
+
+            var _forEvent = new EventViewVM
+            { 
+                IncomingEvent = _pendingEvents,
+                PastEvent = _pastEvents,
+                PendingEvent = _pendingEvents
+            };
+
+            return View(_forEvent);
+
+        }
+
+        [HttpGet]
+        public IActionResult Inventory()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Notification()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reports()
         {
             return View();
         }
