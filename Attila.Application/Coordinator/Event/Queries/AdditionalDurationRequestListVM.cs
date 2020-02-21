e21f9df5f9cd3054a8e0148adf1f6961ad42c91d
Adrian Surla <adrian.surla@uniwallet.ph>
2020-02-21 21:45:53 +0800

    event coordinator queries

---
+++ Attila.Application/Coordinator/Event/Queries/AdditionalDurationRequestListVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class AdditionalDurationRequestListVM
+    {
+        public int ID { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public int EventDetailsID { get; set; }
+    }
+}
--- Attila.Application/Coordinator/Event/Queries/GetEventListQuery.cs
+++ Attila.Application/Coordinator/Event/Queries/GetEventListQuery.cs
@@ -1,19 +1,21 @@
-﻿using Attila.Application.Interfaces;
+﻿using Attila.Application.Coordinator.Event.Queries;
+using Attila.Application.Interfaces;
 using Attila.Domain.Entities.Tables;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Attila.Application.Event.Queries
 {
-    public class GetEventListQuery : IRequest<List<EventDetails>>
+    public class GetEventListQuery : IRequest<IEnumerable<SearchEventVM>>
     {
 
-        public class GetEventListQueryHandler : IRequestHandler<GetEventListQuery, List<EventDetails>>
+        public class GetEventListQueryHandler : IRequestHandler<GetEventListQuery, IEnumerable<SearchEventVM>>
         {
             private readonly IAttilaDbContext dbContext;
             public GetEventListQueryHandler(IAttilaDbContext dbContext)
@@ -21,9 +23,26 @@ namespace Attila.Application.Event.Queries
                 this.dbContext = dbContext;
             }
 
-            public async Task<List<EventDetails>> Handle(GetEventListQuery request, CancellationToken cancellationToken)
+            public async Task<IEnumerable<SearchEventVM>> Handle(GetEventListQuery request, CancellationToken cancellationToken)
             {
-                var _viewEventList = await dbContext.EventsDetails.ToListAsync();
+                var _viewEventList = await dbContext.EventsDetails.Select(a => new SearchEventVM 
+                {
+                    ID = a.ID,
+                    Code = a.Code,
+                    EventName = a.EventName,
+                    Type = a.Type,
+                    EventStatus = a.EventStatus,
+                    Address = a.Address,
+                    BookingDate = a.BookingDate,
+                    EventDate = a.EventDate,
+                    Description = a.Description,
+                    Location = a.Location,
+                    Remarks = a.Remarks,
+                    UserID = a.UserID,
+                    EventPackageDetailsID = a.EventPackageDetailsID,
+                    EventClientID = a.EventClientID
+
+                }).ToListAsync();
 
                 return _viewEventList;
             }
