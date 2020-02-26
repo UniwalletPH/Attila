5b9f65f76ee560bd9bce8cdf32e7dc6517f03a07
Adrian Surla <adrian.surla@uniwallet.ph>
2020-02-26 17:57:48 +0800

    entities

--- Attila/Enums/EventType.cs
+++ Attila/Enums/EventType.cs
@@ -7,7 +7,7 @@ namespace Attila.Domain.Enums
     public enum EventType : byte
     {
         Wedding = 1,
-        Birthday =2,
+        Birthday = 2,
         Others = 3,
         Graduation = 4,
     }
