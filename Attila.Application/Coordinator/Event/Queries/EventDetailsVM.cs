276c79452f78baeb1d9c56a8d5dc32d7a3434b51
Roy Paolo Real <rpreal99@gmail.com>
2020-03-02 15:52:51 +0800

    new event VMs

---
+++ Attila.Application/Coordinator/Event/Queries/EventDetailsVM.cs
@@ -0,0 +1,38 @@
+﻿using Attila.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class EventDetailsVM
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+
+        public string EventName { get; set; }
+
+        public EventType Type { get; set; }
+
+        public Status EventStatus { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime BookingDate { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public string Remarks { get; set; }
+
+        public int UserID { get; set; }
+
+        public int EventPackageDetailsID { get; set; }
+
+        public int EventClientID { get; set; }
+
+
+    }
+}
---
+++ Attila.Application/Coordinator/Event/Queries/EventPackageVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class EventPackageVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int NumberOfGuest { get; set; }
+        [Required]
+        public decimal Rate { get; set; }
+        [Required]
+        public TimeSpan Duration { get; set; }
+    }
+}
