39b4ec71a76e29f1c5db1c46bfc0291ce931393e
Roy Paolo Real <rpreal99@gmail.com>
2020-03-02 15:53:07 +0800

    event models

--- Attila.UI/Models/AddEventPackageVM.cs
+++ Attila.UI/Models/AddEventPackageVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Attila.UI.Models
 {
-    public class EventPackageVM
+    public class AddEventPackageVM
     {
         public int ID { get; set; }
         [Required]
---
+++ Attila.UI/Models/AddEventVM.cs
@@ -0,0 +1,23 @@
+﻿using Attila.Application.Coordinator.Event.Queries;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attila.UI.Models
+{
+    public class AddEventVM
+    {
+
+        public EventDetailsVM Event { get; set; }
+
+        public List<SelectListItem> PackageList { get; set; }
+
+        public List<SelectListItem> ClientList { get; set; }
+
+        public int Selected { get; set; }
+
+        public int SelectedClient { get; set; }
+    }
+}
