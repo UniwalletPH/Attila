04457f6e0efacfbde703a72b03723eb8523fac85
Roy Paolo Real <rpreal99@gmail.com>
2020-03-05 19:01:07 +0800

    coordinator - added new VMs

---
+++ Attila.Application/Coordinator/Event/Queries/MenuCategoryVM.cs
@@ -0,0 +1,19 @@
+﻿using Attila.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class MenuCategoryVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public int PackageDetailsID { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public PackageMenuDetails PackageDetails { get; set; }
+    }
+}
---
+++ Attila.Application/Coordinator/Event/Queries/MenuVM.cs
@@ -0,0 +1,21 @@
+﻿using Attila.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class MenuVM
+    {
+        public int ID { get; set; }
+        [Required]
+        public int MenuCategoryID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public MenuCategoryVM MenuCategory { get; set; }
+    }
+}
