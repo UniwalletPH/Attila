25dfaed28f8e3d322d4a90d63bfc5e00a1421510
Roy Paolo Real <rpreal99@gmail.com>
2020-03-05 19:00:28 +0800

    coordinator - added new commands

--- Attila.Application/Coordinator/Event/Commands/AddMenuCategoryCommand.cs
+++ Attila.Application/Coordinator/Event/Commands/AddMenuCategoryCommand.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Attila.Application.Coordinator.Event.Queries;
+using Attila.Application.Interfaces;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Attila.Application.Coordinator.Event.Commands
 {
-    public class AddMenuCategoryCommand
+    public class AddMenuCategoryCommand : IRequest<bool>
     {
+        public MenuCategoryVM MenuCategory { get; set; }
 
+        public class AddMenuCategoryCommandHandler :IRequestHandler<AddMenuCategoryCommand, bool>
+        {
+            public readonly IAttilaDbContext dbContext;
+            public AddMenuCategoryCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+
+            public async Task<bool> Handle(AddMenuCategoryCommand request, CancellationToken cancellationToken)
+            {
+                var _newMenuCategory = new MenuCategory
+                {
+                    Category = request.MenuCategory.Category,
+                    PackageDetailsID = request.MenuCategory.PackageDetailsID
+                };
+
+                dbContext.MenuCategories.Add(_newMenuCategory);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+        }
     }
 }
--- Attila.Application/Coordinator/Event/Commands/AddMenuCommand.cs
+++ Attila.Application/Coordinator/Event/Commands/AddMenuCommand.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Attila.Application.Coordinator.Event.Queries;
+using Attila.Application.Interfaces;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Attila.Application.Coordinator.Event.Commands
 {
-    class AddMenuCommand
+    public class AddMenuCommand : IRequest<bool>
     {
+        public MenuVM PackageMenu { get; set; }
+
+        public class AddMenuCommandHandler : IRequestHandler<AddMenuCommand, bool>
+        {
+            public readonly IAttilaDbContext dbContext;
+            public AddMenuCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+
+            public async Task<bool> Handle(AddMenuCommand request, CancellationToken cancellationToken)
+            {
+                var _newMenu = new Menu
+                {
+                    Description = request.PackageMenu.Description,
+                    MenuCategoryID = request.PackageMenu.MenuCategoryID,
+                    Name = request.PackageMenu.Name
+                };
+
+                dbContext.Menus.Add(_newMenu);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+        }
     }
 }
