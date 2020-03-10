b1be217384a3c0e295480c2b6e23cc28b690eebc
Roy Paolo Real <rpreal99@gmail.com>
2020-03-10 19:34:41 +0800

    coordinator - added new enums

---
+++ Attila/Enums/LocationType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Domain.Enums
+{
+    public enum LocationType
+    {
+        North = 1,
+        South = 2
+    }
+}
---
+++ Attila/Enums/VenueType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Domain.Enums
+{
+    public enum VenueType : byte
+    {
+        Building = 1,
+        Condominium = 2
+    }
+}
