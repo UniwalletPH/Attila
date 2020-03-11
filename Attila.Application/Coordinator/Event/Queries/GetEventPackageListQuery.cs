acd6ca4d82d33c7b5407d7402658f7bda272f5d2
Roy Paolo Real <rpreal99@gmail.com>
2020-03-11 19:45:45 +0800

    coordinator - added GetEventPackageListQuery

---
+++ Attila.Application/Coordinator/Event/Queries/GetEventPackageListQuery.cs
@@ -0,0 +1,40 @@
+﻿using Attila.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Coordinator.Event.Queries
+{
+    public class GetEventPackageListQuery : IRequest<IEnumerable<EventPackageVM>>
+    {
+        public class GetEventPackageListQueryHandler : IRequestHandler<GetEventPackageListQuery, IEnumerable<EventPackageVM>>
+        {
+            private readonly IAttilaDbContext dbContext;
+
+            public GetEventPackageListQueryHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<IEnumerable<EventPackageVM>> Handle(GetEventPackageListQuery request, CancellationToken cancellationToken)
+            {
+                var _viewEventPackageList = await dbContext.PackageMenuDetails.Select(a => new EventPackageVM
+                {
+                   Code = a.Code,
+                   Description = a.Description,
+                   Name = a.Name,
+                   RatePerHead = a.RatePerHead,
+                   ID = a.ID
+                   
+                }).ToListAsync();
+
+                return _viewEventPackageList;
+            }
+        }
+    }
+}
