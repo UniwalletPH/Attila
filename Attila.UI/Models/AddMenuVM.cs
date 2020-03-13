ad95da6d3c3585365759b758c585b0efb13a9d65
Roy Paolo Real <rpreal99@gmail.com>
2020-03-13 20:09:14 +0800

    coordinator - added UI model

---
+++ Attila.UI/Models/AddMenuVM.cs
@@ -0,0 +1,18 @@
+﻿using Attila.Application.Coordinator.Event.Queries;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attila.UI.Models
+{
+    public class AddMenuVM
+    {
+        public MenuVM Menu { get; set; }
+
+        public List<SelectListItem> CategoryList { get; set; }
+
+        public int Selected { get; set; }
+    }
+}
