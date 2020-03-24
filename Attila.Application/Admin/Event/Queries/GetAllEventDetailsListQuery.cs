cdf3e9f32ca1034fb4f36ab21b22259fc08837c4
Jeeeeeyo <47585152+Jeeeeeyo@users.noreply.github.com>
2020-03-25 00:55:07 +0800

    EventList

--- Attila.Application/Admin/Event/Queries/GetAllEventDetailsListQuery.cs
+++ Attila.Application/Admin/Event/Queries/GetAllEventDetailsListQuery.cs
@@ -1,20 +1,22 @@
-﻿using Attila.Application.Interfaces;
+﻿using Attila.Application.Admin.Event.Queries;
+using Attila.Application.Interfaces;
 using Attila.Domain.Entities;
 using Attila.Domain.Entities.Tables;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Attila.Application.Admin.Queries
 {
-    public class GetAllEventDetailsListQuery : IRequest<List<EventDetails>>
+    public class GetAllEventDetailsListQuery : IRequest<List<EventVM>>
     {
        
-        public class ViewAllEventDetailsListQueryHandler : IRequestHandler<GetAllEventDetailsListQuery, List<EventDetails>>
+        public class ViewAllEventDetailsListQueryHandler : IRequestHandler<GetAllEventDetailsListQuery, List<EventVM>>
         {
             private readonly IAttilaDbContext dbContext;
 
@@ -24,11 +26,37 @@ namespace Attila.Application.Admin.Queries
                 this.dbContext = dbContext;
             }
 
-            public Task<List<EventDetails>> Handle(GetAllEventDetailsListQuery request, CancellationToken cancellationToken)
+            public async Task<List<EventVM>> Handle(GetAllEventDetailsListQuery request, CancellationToken cancellationToken)
             {
-                var _listOfEvents = dbContext.EventDetails.ToListAsync();
+                
+                var _listIncomingEvents = new List<EventVM>();
 
-                return _listOfEvents;
+                var _incomingEvents = dbContext.EventDetails
+                    .Include(a => a.PackageDetails)
+                    .Include(a => a.EventClient)
+                    .Include(a => a.User).ToList();
+
+                foreach (var item in _incomingEvents)
+                {
+                    var Event = new EventVM
+                    {
+                        ID = item.ID,
+                        EventName = item.EventName,
+                        Location = item.Location,
+                        BookingDate = item.BookingDate,
+                        EventDate = item.EventDate,
+                        Description = item.Description,
+                        Type = item.Type,
+                        Package = item.PackageDetails,
+                        Coordinator = item.User,
+                        Client = item.EventClient,
+                        EventStatus = item.EventStatus,
+                        Remarks = item.Remarks
+                    };
+
+                    _listIncomingEvents.Add(Event);
+                }
+                return _listIncomingEvents;
             }
         }
     }
--- Attila.UI/Controllers/AdminController.cs
+++ Attila.UI/Controllers/AdminController.cs
@@ -1,149 +1,149 @@
-﻿using Atilla.Application.Admin.Equipment.Commands;
-using Attila.Application.Admin.Commands;
-using Attila.Application.Admin.Equipment.Commands;
-using Attila.Application.Admin.Event.Queries;
-using Attila.Application.Admin.Food.Commands;
-using Attila.Application.Admin.Food.Queries;
-using Attila.Application.Admin.Inventory.Queries;
-using Attila.Application.Users.Commands;
-using Attila.Application.Users.Queries;
-using Attila.UI.Models;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿//using Atilla.Application.Admin.Equipment.Commands;
+//using Attila.Application.Admin.Commands;
+//using Attila.Application.Admin.Equipment.Commands;
+//using Attila.Application.Admin.Event.Queries;
+//using Attila.Application.Admin.Food.Commands;
+//using Attila.Application.Admin.Food.Queries;
+//using Attila.Application.Admin.Inventory.Queries;
+//using Attila.Application.Users.Commands;
+//using Attila.Application.Users.Queries;
+//using Attila.UI.Models;
+//using MediatR;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Threading.Tasks;
 
-namespace Attila.UI.Controllers
-{
-    public class AdminController : Controller
-    {
-        private readonly IMediator mediator;
+//namespace Attila.UI.Controllers
+//{
+//    public class AdminController : Controller
+//    {
+//        private readonly IMediator mediator;
 
-        public AdminController(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
+//        public AdminController(IMediator mediator)
+//        {
+//            this.mediator = mediator;
+//        }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
 
         
-        public async Task<IActionResult> Event()
-        {
-            var _pendingEvents = await mediator.Send(new GetAllPendingEventsQuery { });
-            var _incomingEvents = await mediator.Send(new GetAllIncomingEventsQuery { });
-            var _pastEvents = await mediator.Send(new GetAllPastEventsQuery { });
+//        public async Task<IActionResult> Event()
+//        {
+//            var _pendingEvents = await mediator.Send(new GetAllPendingEventsQuery { });
+//            var _incomingEvents = await mediator.Send(new GetAllIncomingEventsQuery { });
+//            var _pastEvents = await mediator.Send(new GetAllPastEventsQuery { });
 
-            var _forEvent = new EventViewVM
-            { 
-                IncomingEvent = _incomingEvents,
-                PastEvent = _pastEvents,
-                PendingEvent = _pendingEvents
-            };
+//            var _forEvent = new EventViewVM
+//            { 
+//                IncomingEvent = _incomingEvents,
+//                PastEvent = _pastEvents,
+//                PendingEvent = _pendingEvents
+//            };
 
-            return View(_forEvent);
-        }
+//            return View(_forEvent);
+//        }
 
-       [HttpPost]
-        public async Task<IActionResult> ViewEvent(int EventID)
-        {
-            var _eventDetails = await mediator.Send(new GetEventDetailQuery { EventID = EventID});
+//       [HttpPost]
+//        public async Task<IActionResult> ViewEvent(int EventID)
+//        {
+//            var _eventDetails = await mediator.Send(new GetEventDetailQuery { EventID = EventID});
 
-            var _allEventDetails = new ViewEventVM
-            {                 
-                EventDetails = _eventDetails
-            };
+//            var _allEventDetails = new ViewEventVM
+//            {                 
+//                EventDetails = _eventDetails
+//            };
 
-            return PartialView("~/Views/Admin/Partial/_viewEvent.cshtml", _allEventDetails);
-        }
-
-
-
-        public async Task<IActionResult> ApproveEvent (int eventID)
-        {
-            var _retVal = await mediator.Send(new ApproveEventRequestCommand { EventID = eventID});
-
-            return Json(_retVal);
-
-        }
-
-        public async Task<IActionResult> DeclineEvent(int eventID)
-        {
-            var _retVal = await mediator.Send(new DeclineEventRequestCommand { EventID = eventID});
-
-            return Json(_retVal);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Inventory()
-        {
-            var _pendingFoodReq = await mediator.Send(new GetPendingFoodRestockRequestQuery { });
-            var _pendingEquimentReq = await mediator.Send(new GetPendingEquipmentRestockRequestQuery { });
-
-            var _pendingRequest = new InventoryViewVM
-            { 
-            PendingFoodRestockRequest = _pendingFoodReq,
-            PendingEquipmentRestockRequest = _pendingEquimentReq
-            };
-
-            return View(_pendingRequest);
-        }
-
-        public async Task<IActionResult> ApproveFoodRestockRequest(int foodID)
-        {
-            var _return = await mediator.Send(new ApproveFoodRestockRequestCommand { RequestID = foodID});
-
-            return Json(_return);
-        }
-
-        public async Task<IActionResult> DeclineFoodRestockRequest(int foodID)
-        {
-            var _return = await mediator.Send(new DeclineFoodRestockRequestCommand { RequestID = foodID});
-
-            return Json(_return);
-        }
-
-        public async Task<IActionResult> ApproveEquipmentRestockRequest(int equipmentID)
-        {
-
-            var _return = await mediator.Send(new ApproveEquipmentRestockRequestCommand { RequestID = equipmentID});
-
-            return Json(_return);
-        }
-        public async Task<IActionResult> DeclineEquipmentRestockRequest(int equipmentID)
-        {
-
-            var _return = await mediator.Send(new DeclineEquipmentRestockRequestCommand {RequestID = equipmentID } );
-
-            return Json(_return);
-        }
+//            return PartialView("~/Views/Admin/Partial/_viewEvent.cshtml", _allEventDetails);
+//        }
 
 
-        public IActionResult Profile()
-        {
-            return View();
-        }
 
-        public async Task<IActionResult> SaveUser(UserVM user)
-        {
-            var _return = await mediator.Send(new AddUserCommand { User = user});
+//        public async Task<IActionResult> ApproveEvent (int eventID)
+//        {
+//            var _retVal = await mediator.Send(new ApproveEventRequestCommand { EventID = eventID});
 
-            return Json(true);
-        }
+//            return Json(_retVal);
+
+//        }
+
+//        public async Task<IActionResult> DeclineEvent(int eventID)
+//        {
+//            var _retVal = await mediator.Send(new DeclineEventRequestCommand { EventID = eventID});
+
+//            return Json(_retVal);
+//        }
+
+//        [HttpGet]
+//        public async Task<IActionResult> Inventory()
+//        {
+//            var _pendingFoodReq = await mediator.Send(new GetPendingFoodRestockRequestQuery { });
+//            var _pendingEquimentReq = await mediator.Send(new GetPendingEquipmentRestockRequestQuery { });
+
+//            var _pendingRequest = new InventoryViewVM
+//            { 
+//            PendingFoodRestockRequest = _pendingFoodReq,
+//            PendingEquipmentRestockRequest = _pendingEquimentReq
+//            };
+
+//            return View(_pendingRequest);
+//        }
+
+//        public async Task<IActionResult> ApproveFoodRestockRequest(int foodID)
+//        {
+//            var _return = await mediator.Send(new ApproveFoodRestockRequestCommand { RequestID = foodID});
+
+//            return Json(_return);
+//        }
+
+//        public async Task<IActionResult> DeclineFoodRestockRequest(int foodID)
+//        {
+//            var _return = await mediator.Send(new DeclineFoodRestockRequestCommand { RequestID = foodID});
+
+//            return Json(_return);
+//        }
+
+//        public async Task<IActionResult> ApproveEquipmentRestockRequest(int equipmentID)
+//        {
+
+//            var _return = await mediator.Send(new ApproveEquipmentRestockRequestCommand { RequestID = equipmentID});
+
+//            return Json(_return);
+//        }
+//        public async Task<IActionResult> DeclineEquipmentRestockRequest(int equipmentID)
+//        {
+
+//            var _return = await mediator.Send(new DeclineEquipmentRestockRequestCommand {RequestID = equipmentID } );
+
+//            return Json(_return);
+//        }
 
 
-        [HttpGet]
-        public IActionResult Notification()
-        {
-            return View();
-        }
+//        public IActionResult Profile()
+//        {
+//            return View();
+//        }
 
-        [HttpGet]
-        public IActionResult Reports()
-        {
-            return View();
-        }
+//        public async Task<IActionResult> SaveUser(UserVM user)
+//        {
+//            var _return = await mediator.Send(new AddUserCommand { User = user});
 
-    }
-}
+//            return Json(true);
+//        }
+
+
+//        [HttpGet]
+//        public IActionResult Notification()
+//        {
+//            return View();
+//        }
+
+//        [HttpGet]
+//        public IActionResult Reports()
+//        {
+//            return View();
+//        }
+
+//    }
+//}
