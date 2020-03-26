8789694df6cc92a6eb3992bcb73642025f6eb2c6
Adrian Surla <adrian.surla@uniwallet.ph>
2020-03-26 22:24:53 +0800

    inventory manager - added query and VM

---
+++ Attila.Application/Inventory Manager/Shared/Queries/GetInventoryQuery.cs
@@ -0,0 +1,49 @@
+﻿using Attila.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Inventory_Manager.Shared.Queries
+{
+    public class GetInventoryQuery : IRequest<IEnumerable<InventoriesVM>>
+    {
+        public class GetInventoryQueryHandler : IRequestHandler<GetInventoryQuery, IEnumerable<InventoriesVM>>
+        {
+            private readonly IAttilaDbContext dbContext;
+            private readonly IMediator mediator;
+            public GetInventoryQueryHandler(IAttilaDbContext dbContext, IMediator mediator)
+            {
+                this.dbContext = dbContext;
+                this.mediator = mediator;
+            }
+            public async Task<IEnumerable<InventoriesVM>> Handle(GetInventoryQuery request, CancellationToken cancellationToken)
+            {
+                var _inventoryDataList = new List<InventoriesVM>();
+
+                var _getInventoryData = dbContext.EquipmentInventories.Include(a => a.EquipmentDetails);
+
+                foreach (var item in _getInventoryData)
+                {
+                    var _inventoryAllDetails = new InventoriesVM
+                    {
+                        ID = item.ID,
+                        Quantity = item.Quantity,
+                        EncodingDate = item.EncodingDate,
+                        ItemPrice = item.ItemPrice,
+                        Remarks = item.Remarks,
+                        UserID = item.UserID,
+                        EquipmentDetails = item.EquipmentDetails
+                    };
+
+                    _inventoryDataList.Add(_inventoryAllDetails);
+                }
+
+                return _inventoryDataList;
+            }
+        }
+    }
+}
---
+++ Attila.Application/Inventory Manager/Shared/Queries/InventoriesVM.cs
@@ -0,0 +1,27 @@
+﻿using Attila.Application.Inventory_Manager.Equipment.Queries;
+using Attila.Domain.Entities;
+using Attila.Domain.Entities.Enums;
+using Attila.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Application.Inventory_Manager.Shared.Queries
+{
+    public class InventoriesVM
+    {
+        public int ID { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime EncodingDate { get; set; }
+
+        public decimal ItemPrice { get; set; }
+
+        public string Remarks { get; set; }
+
+        public int UserID { get; set; }
+
+        public EquipmentDetails EquipmentDetails { get; set; }
+    }
+}
