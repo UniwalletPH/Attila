2663a4a777136350f28515c9cabe1dccd5cc12f1
Roy Paolo Real <rpreal99@gmail.com>
2020-03-30 17:02:24 +0800

    coordinator - check in

--- Attila.UI/Controllers/PackagesController.cs
+++ Attila.UI/Controllers/PackagesController.cs
@@ -394,7 +394,7 @@ namespace Attila.UI.Controllers
             bool flag = true;
             MenuCategoryVM _menuCategory = new MenuCategoryVM
             {
-                //Category = _menu.MenuCategory.Category,
+                Category = _menu.Category,
             };
             try
             {
