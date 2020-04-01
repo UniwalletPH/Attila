53b6f83d3d5fe47bced7b47ccbf72b89961ca308
Adrian Surla <adrian.surla@uniwallet.ph>
2020-04-01 14:51:14 +0800

    changed Enums' namespace to "Attila" only

--- Attila/Enums/FoodType.cs
+++ Attila/Enums/FoodType.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Attila.Domain.Entities.Enums
+namespace Attila
 {
     public enum FoodType : byte
     {
--- Attila/Enums/UnitType.cs
+++ Attila/Enums/UnitType.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Attila.Domain.Enums
+namespace Attila
 {
     public enum UnitType : byte
     {
