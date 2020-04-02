9e817d3b131a2347e6543d88aa0f3893217689af
Anthony Jasper Dalit <anthony.dalit@uniwallet.ph>
2020-04-02 16:38:41 +0800

    Added new Migration - Client Table

---
+++ Attila.DbMigration/Migrations/20200402082859_ModifiedClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class ModifiedClient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Clients",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
