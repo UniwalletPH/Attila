53547bf73972466974a619c13dab8bbbba3651e4
Adrian Surla <adrian.surla@uniwallet.ph>
2020-04-03 22:16:19 +0800

    Added association class for Equipment and EventAdditionalEquipmentRequest

---
+++ Attila/Entities/EventEquipmentRequestCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Attila.Domain.Entities
+{
+    public class EventEquipmentRequestCollection
+    {
+        [ForeignKey("Equipment")]
+        public int EquipmentID { get; set; }
+        [ForeignKey("EventAdditionalEquipmentRequest")]
+        public int EventAdditionalEquipmentRequestID { get; set; }
+
+
+        public Equipment Equipment { get; set; }
+        public EventAdditionalEquipmentRequest EventAdditionalEquipmentRequest { get; set; }
+    }
+}
