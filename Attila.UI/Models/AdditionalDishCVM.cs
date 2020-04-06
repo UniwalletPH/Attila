6644d033e6d85f7c2d194b259cff6a2dd3d7a7e6
Roy Paolo Real <rpreal99@gmail.com>
2020-04-06 12:56:10 +0800

    added a CVM for AdditionalDishes

---
+++ Attila.UI/Models/AdditionalDishCVM.cs
@@ -0,0 +1,18 @@
+﻿using Attila.Application.Coordinator.Events.Queries;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attila.UI.Models
+{
+    public class AdditionalDishCVM
+    {
+        public AdditionalDishVM AdditionalDish { get; set; }
+        public List<SelectListItem> DishList { get; set; }
+        public List<SelectListItem> EventList { get; set; }
+        public int SelectedDish { get; set; }
+        public int SelectedEvent { get; set; }
+    }
+}
