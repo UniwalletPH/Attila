9a72568ce304231146f6395471f1ff56dcf42a40
Roy Paolo Real <rpreal99@gmail.com>
2020-04-06 12:39:22 +0800

    coordinator - added new commands

---
+++ Attila.Application/Coordinator/Events/Commands/AddEventDishRequestCommand.cs
@@ -0,0 +1,41 @@
+﻿using Attila.Application.Coordinator.Events.Queries;
+using Attila.Application.Interfaces;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Coordinator.Events.Commands
+{
+    public class AddEventDishRequestCommand : IRequest<bool>
+    {
+        public List<EventDishRequestVM> EventDishRequest { get; set; }
+        public class AddEventDishRequestCommandHandler : IRequestHandler<AddEventDishRequestCommand, bool>
+        {
+            public readonly IAttilaDbContext dbContext;
+
+            public AddEventDishRequestCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<bool> Handle(AddEventDishRequestCommand request, CancellationToken cancellationToken)
+            {
+                foreach (var item in request.EventDishRequest)
+                {
+                    var EventDishRequests = new EventDishRequest
+                    {
+                        AdditionalDishID = item.AdditionalDishID,
+                        DishID = item.DishID
+                    };
+                    dbContext.EventDishRequests.Add(EventDishRequests);
+                    await dbContext.SaveChangesAsync();
+                }
+                return true;
+            }
+        }
+    }
+}
