c319573c757bf13a02ac4de32179a811e779b7a2
Adrian Surla <adrian.surla@uniwallet.ph>
2020-04-27 20:13:27 +0800

    Inventory Manager - separeted request and collection command

---
+++ Attila.Application/Inventory Manager/Foods/Commands/AddFoodRequestCollectionCommand.cs
@@ -0,0 +1,46 @@
+﻿using Attila.Application.Interfaces;
+using Attila.Application.Inventory_Manager.Foods.Queries;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Inventory_Manager.Foods.Commands
+{
+    public class AddFoodRequestCollectionCommand : IRequest<bool>
+    {
+        public int FoodRestockID { get; set; }
+
+        public FoodsRestockRequestVM MyFoodRestockRequestVM { get; set; }
+
+        public class AddFoodRequestCollectionCommandHandler : IRequestHandler<AddFoodRequestCollectionCommand, bool>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public AddFoodRequestCollectionCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+            public async Task<bool> Handle(AddFoodRequestCollectionCommand request, CancellationToken cancellationToken)
+            {
+                foreach (var item in request.MyFoodRestockRequestVM.FoodRequestCollection)
+                {
+                    FoodRequestCollection _foodRequestCollection = new FoodRequestCollection
+                    {
+                        FoodID = item.FoodID,
+                        FoodRestockRequestID = request.FoodRestockID,
+                        Quantity = item.Quantity
+                    };
+
+                    dbContext.FoodRequestCollections.Add(_foodRequestCollection);
+                }
+
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Attila.Application/Inventory Manager/Foods/Commands/AddFoodRequestCommand.cs
@@ -0,0 +1,41 @@
+﻿using Attila.Application.Interfaces;
+using Attila.Application.Inventory_Manager.Foods.Queries;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Inventory_Manager.Foods.Commands
+{
+    public class AddFoodRequestCommand : IRequest<int>
+    {
+        public FoodsRestockRequestVM MyFoodRestockRequestVM { get; set; }
+
+        public class AddFoodRequestCommandHandler : IRequestHandler<AddFoodRequestCommand, int>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public AddFoodRequestCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<int> Handle(AddFoodRequestCommand request, CancellationToken cancellationToken)
+            {
+                FoodRestockRequest _foodRestockRequest = new FoodRestockRequest
+                {
+                    DateTimeRequest = DateTime.Now,
+                    Status = Status.ForApproval,
+                    InventoryManagerID = request.MyFoodRestockRequestVM.UserID
+                };
+
+                dbContext.FoodRestockRequests.Add(_foodRestockRequest);
+                await dbContext.SaveChangesAsync();
+
+                return _foodRestockRequest.ID;
+            }
+        }
+    }
+}
