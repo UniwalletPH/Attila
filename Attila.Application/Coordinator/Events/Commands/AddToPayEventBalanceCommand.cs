e2f402533f6a6cd863509bdb38c716169c81c45c
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-01 13:26:31 +0800

    Admin - added AddToPayBalanceCommand

---
+++ Attila.Application/Coordinator/Events/Commands/AddToPayEventBalanceCommand.cs
@@ -0,0 +1,43 @@
+﻿using Attila.Application.Coordinator.Events.Queries;
+using Attila.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Coordinator.Events.Commands
+{
+    public class AddToPayEventBalanceCommand : IRequest<int>
+    {
+        public EventDetailsVM MyEventDetailsVM { get; set; }
+
+        public class AddToPayEventBalanceCommandHandler : IRequestHandler<AddToPayEventBalanceCommand, int>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public AddToPayEventBalanceCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<int> Handle(AddToPayEventBalanceCommand request, CancellationToken cancellationToken)
+            {
+                var _getEvent = dbContext.Events.Find(request.MyEventDetailsVM.ID);
+
+                if (_getEvent != null)
+                {
+                    _getEvent.ToPay += request.MyEventDetailsVM.ToPay;
+                    await dbContext.SaveChangesAsync();
+
+                    return request.MyEventDetailsVM.ID;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+            }
+        }
+    }
+}
