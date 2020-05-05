9885e16eae4f7400ce02daa2536b216342671a79
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-05 08:43:23 +0800

    Coordinator - added "AddAdditionalEventChargeCommand"

---
+++ Attila.Application/Coordinator/Events/Commands/AddAdditionalEventChargeCommand.cs
@@ -0,0 +1,44 @@
+﻿using Attila.Application.Coordinator.Events.Queries;
+using Attila.Application.Interfaces;
+using Attila.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Coordinator.Events.Commands
+{
+    public class AddAdditionalEventChargeCommand : IRequest<bool>
+    {
+        public AdditionalEventFeeVM MyAdditionalEventFeeVM  { get; set; }
+
+        public class AddAdditionalEventChargeCommandHandler : IRequestHandler<AddAdditionalEventChargeCommand, bool>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public AddAdditionalEventChargeCommandHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<bool> Handle(AddAdditionalEventChargeCommand request, CancellationToken cancellationToken)
+            {
+                EventFee _additionalEventFee = new EventFee
+                {
+                    EventID = request.MyAdditionalEventFeeVM.EventID,
+                    Description = request.MyAdditionalEventFeeVM.Description,
+                    Item = request.MyAdditionalEventFeeVM.Item,
+                    Quantity = request.MyAdditionalEventFeeVM.Quantity,
+                    PricePerQuantity = request.MyAdditionalEventFeeVM.PricePerQuantity,
+                    TotalPrice = request.MyAdditionalEventFeeVM.PricePerQuantity * request.MyAdditionalEventFeeVM.Quantity
+                };
+
+                dbContext.EventFees.Add(_additionalEventFee);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Attila.Application/Coordinator/Events/Queries/AdditionalEventFeeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Application.Coordinator.Events.Queries
+{
+    public class AdditionalEventFeeVM
+    {
+        public int EventID { get; set; }
+
+        public string Description { get; set; }
+
+        public string Item { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PricePerQuantity { get; set; }
+    }
+}
