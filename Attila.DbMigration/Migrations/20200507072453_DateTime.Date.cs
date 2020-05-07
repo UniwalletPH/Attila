26cebb054809407a94b1199685589a0a8c29c747
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-07 18:02:33 +0800

    Added new migration - changed some DateTime to Date only

---
+++ Attila.DbMigration/Migrations/20200507072453_DateTime.Date.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class DateTimeDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Deliveries",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Deliveries",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
