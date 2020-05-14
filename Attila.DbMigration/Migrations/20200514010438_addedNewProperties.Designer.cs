be21f5eaa9d19468f86b1c145ba143befec0b691
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-14 09:04:53 +0800

    Added new migration

--- Attila.DbMigration/Migrations/20200514010438_addedNewProperties.Designer.cs
+++ Attila.DbMigration/Migrations/20200514010438_addedNewProperties.Designer.cs
@@ -4,14 +4,16 @@ using Attila.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Attila.DbMigration.Migrations
 {
     [DbContext(typeof(AttilaDbContext))]
-    partial class AttilaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200514010438_addedNewProperties")]
+    partial class addedNewProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Attila.DbMigration/Migrations/20200514010438_addedNewProperties.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class addedNewProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Messages",
+                table: "Notifications",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EstimatedPrice",
+                table: "EquipmentRequestCollections",
+                type: "DECIMAL(20,8)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Messages",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "EstimatedPrice",
+                table: "EquipmentRequestCollections");
+        }
+    }
+}
