09e324a994f1b8f0c59b6cfcac34d7eefc47f19c
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-14 14:20:38 +0800

    Added new migration

---
+++ Attila.DbMigration/Migrations/20200514055634_AddedEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class AddedEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "EstimatedPrice",
+                table: "FoodRequestCollections",
+                type: "DECIMAL(20,8)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedPrice",
+                table: "FoodRequestCollections");
+        }
+    }
+}
