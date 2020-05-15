c24bb6850f7a13e97d9c98bc6b5a06e4d9f672e2
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-15 09:24:28 +0800

    Added new migration

---
+++ Attila.DbMigration/Migrations/20200514112457_AddedRemarksProperty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class AddedRemarksProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Remarks",
+                table: "FoodRestockRequests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Remarks",
+                table: "FoodRestockRequests");
+        }
+    }
+}
