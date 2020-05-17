9745f8a96810d0384413efa54448de165fc31be8
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-17 11:52:13 +0800

    Inventory Manager - added new entity "EquipmentTracking"

---
+++ Attila/Entities/EquipmentTracking.cs
@@ -0,0 +1,25 @@
+﻿using Attila.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila.Domain.Entities
+{
+    public class EquipmentTracking : BaseAuditedEntity
+    {
+        public int EventID { get; set; }
+        public int EventAdditionalEquipmentRequestID { get; set; }
+        public int EquipmentID { get; set; }
+        public int UserID { get; set; }
+
+        public int Quantity { get; set; }
+        public DateTime TrackingDate { get; set; }
+        public EquipmentAction TrackingAction { get; set; }
+        public string Remarks { get; set; }
+
+        public Event Event { get; set; }
+        public EventAdditionalEquipmentRequest EventAdditionalEquipmentRequest { get; set; }
+        public Equipment Equipment { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Attila/Enums/EquipmentAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attila
+{
+    public enum EquipmentAction : byte
+    {
+        None = 0,
+        CheckIn = 1,
+        CheckOut = 2
+    }
+}
