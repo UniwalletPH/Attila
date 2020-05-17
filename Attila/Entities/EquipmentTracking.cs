3ac08cb68f84a33d4c1491ad9be7665e7b6cc473
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-17 17:53:27 +0800

    added ForeignKeyAttributes

--- Attila/Entities/EquipmentTracking.cs
+++ Attila/Entities/EquipmentTracking.cs
@@ -1,15 +1,23 @@
 ﻿using Attila.Domain.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Attila.Domain.Entities
 {
     public class EquipmentTracking : BaseAuditedEntity
     {
+        [ForeignKey("Event")]
         public int EventID { get; set; }
+
+        [ForeignKey("EventAdditionalEquipmentRequest")]
         public int EventAdditionalEquipmentRequestID { get; set; }
+
+        [ForeignKey("Equipment")]
         public int EquipmentID { get; set; }
+
+        [ForeignKey("User")]
         public int UserID { get; set; }
 
         public int Quantity { get; set; }
