a8ebdc197f92c9d0e314e274ec1c5fc223d3c4e5
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-18 10:33:59 +0800

    Added new migration

--- Attila.DbMigration/Migrations/20200518023244_QuantityProperty.Designer.cs
+++ Attila.DbMigration/Migrations/20200518023244_QuantityProperty.Designer.cs
@@ -4,14 +4,16 @@ using Attila.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Attila.DbMigration.Migrations
 {
     [DbContext(typeof(AttilaDbContext))]
-    partial class AttilaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200518023244_QuantityProperty")]
+    partial class QuantityProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Attila.DbMigration/Migrations/20200518023244_QuantityProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class QuantityProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "EventEquipments",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "EventEquipments");
+        }
+    }
+}
