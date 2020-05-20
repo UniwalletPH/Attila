6ce628e7265abd2a4fd9cb991d40055e00a2648d
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-20 19:02:38 +0800

    Added new query

---
+++ Attila.Application/Coordinator/Events/Queries/GetEventDetailsQuery.cs
@@ -0,0 +1,36 @@
+﻿using Attila.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attila.Application.Coordinator.Events.Queries
+{
+    public class GetEventDetailsQuery : IRequest<EventDetailsVM>
+    {
+        public int EventDetailsID { get; set; }
+        public class GetEventDetailsQueryHandler : IRequestHandler<GetEventDetailsQuery, EventDetailsVM>
+        {
+            private readonly IAttilaDbContext dbContext;
+            public GetEventDetailsQueryHandler(IAttilaDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<EventDetailsVM> Handle(GetEventDetailsQuery request, CancellationToken cancellationToken)
+            {
+                var _getEventDetails = dbContext.Events.Find(request.EventDetailsID);
+
+                EventDetailsVM _eventDetailsVM = new EventDetailsVM
+                {
+                    EventName = _getEventDetails.EventName,
+                    EventStatus = _getEventDetails.EventStatus
+                };
+
+                return _eventDetailsVM;
+            }
+        }
+    }
+}
