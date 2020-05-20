42f5c4a754d74882091e12561fcba793e5b37458
Adrian Surla <adrian.surla@uniwallet.ph>
2020-05-20 19:01:50 +0800

    Added new migration

--- Attila.DbMigration/Migrations/20200520094027_ReturnedProperty.Designer.cs
+++ Attila.DbMigration/Migrations/20200520094027_ReturnedProperty.Designer.cs
@@ -4,14 +4,16 @@ using Attila.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Attila.DbMigration.Migrations
 {
     [DbContext(typeof(AttilaDbContext))]
-    partial class AttilaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200520094027_ReturnedProperty")]
+    partial class ReturnedProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Attila.DbMigration/Migrations/20200520094027_ReturnedProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attila.DbMigration.Migrations
+{
+    public partial class ReturnedProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Returned",
+                table: "EquipmentTracking",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Returned",
+                table: "EquipmentTracking");
+        }
+    }
+}
