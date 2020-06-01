6db703a11fd003653365bec93a9f5157cb91d084
Adrian Surla <adrian.surla@uniwallet.ph>
2020-06-01 10:15:16 +0800

    fixed and added new enums

---
+++ Attila/Enums/ServiceType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Attila.Domain.Enums
+{
+    public enum ServiceType : byte
+    {
+        None = 0,
+
+        [Display(Name = "Food Trays")]
+        FoodTrays = 1,
+        Catering = 2,
+
+        [Display(Name = "Packed Meals")]
+        PackedMeals = 3,
+        Merchandise = 4
+    }
+}
